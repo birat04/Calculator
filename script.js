2ab8ca588a54d24795d29e6acafbe2ffffb3996d
Your Name <you@example.com>
2024-04-16 09:09:38 +0530

    change the size of the cal

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function clearDisplay() {
 
 function calculateDisplay() {
     try {
-        display.value = float(eval(display.value));
+        display.value = eval(display.value);
     }
     catch (error) {
         display.value = "Error";
